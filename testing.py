fa9a6bdf5cc603112e4075625132d59ee681fb43
PrithviRajReddy24 <71643835+PrithviRajReddy24@users.noreply.github.com>
2021-09-03 15:22:51 +0530

    Add files via upload

---
+++ testing.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  3 13:14:04 2021
+
+@author: PRITHVIRAJ REDDY
+"""
+import unittest
+class TestCaseDemo(unittest.TestCase):
+    def setUp(self):
+        print("setup method execution")
+    def test(Self):
+        print("test method execution")
+    def tearDown(self):
+        print("teardown method execution ")
+unittest.main()
+print()
+    
+
+class TestCaseDemo(unittest.TestCase):
+    def setUp(self):
+        print("setup method execution")
+    def test_method1(Self):
+        print("test_method1 execution")
+    def test_method2(self):
+        print("test_method2 execution")
+    def tearDown(self):
+        print("tesrdown methos execution")
+unittest.main()
+print()
+
+
+
+class TestCaseDemo(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        print("setup class method execution")
+    def setUp(self):
+        print("setup method execution")
+    def test_method1(self):
+        print("test_method1 execution")
+    def test_method2(self):
+        print("test_method2 execution")
+    def tearDown(self):
+        print("teardown method execution")
+    @classmethod
+    def tearDownClass(cls):
+        print("teardown class method execution")   
+unittest.main()
+print()
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
